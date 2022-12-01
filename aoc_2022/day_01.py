bf0914abf0ad13486ca1da00666c7fc49aeb5367
philswatton <philswatton@gmail.com>
2022-12-01 10:44:44 +0000

    fully working version

--- aoc_2022/day_01.py
+++ aoc_2022/day_01.py
@@ -1,16 +1,20 @@
 import re
 
+# Iterate over input file, calculate list of elf total calories
 elfList = [0]
-elfIndex = 0
+iterator = 0
 with open("aoc_2022/01_list.txt") as inputFile:
     for line in inputFile.readlines():
         lineRegex = re.search("^\d+", line)
         if lineRegex == None:
-            elfIndex += 1
+            iterator += 1 #assuming only one new line between elves
             elfList.append(0)
         else:
-            elfList[elfIndex] = elfList[elfIndex] + float(lineRegex.group(0))
-            
-print(elfIndex)
-print(elfList)
+            elfList[iterator] = elfList[iterator] + float(lineRegex.group(0))
+
+# Get index of highest calorie elf
+elfIndex = elfList.index(max(elfList))
+
+# Print output to console
+print("Elf " + str(elfIndex+1) + " is carrying the most calories")
         
