b4a99402577e6af6fd126f42684df6db16831bd7
philswatton <philswatton@gmail.com>
2022-12-01 10:58:29 +0000

    now a command line program

--- aoc_2022/01_calories.py
+++ aoc_2022/01_calories.py
@@ -1,20 +1,37 @@
+from argparse import ArgumentParser
 import re
 
-# Iterate over input file, calculate list of elf total calories
-elfList = [0]
-iterator = 0
-with open("aoc_2022/01_list.txt") as inputFile:
-    for line in inputFile.readlines():
-        lineRegex = re.search("^\d+", line)
-        if lineRegex == None:
-            iterator += 1 #assuming only one new line between elves
-            elfList.append(0)
-        else:
-            elfList[iterator] = elfList[iterator] + float(lineRegex.group(0))
+# Usage: python 01_calories.py input_file_path
+def process():
+    parser = ArgumentParser(description="Calculate elf with the most calories")
+    parser.add_argument("input_file_path") # required (positional) arguments
+    args = parser.parse_args()
+    print(args)
+    return args
 
-# Get index of highest calorie elf
-elfIndex = elfList.index(max(elfList))
+# Main
+def main():
+    # Get file path
+    file_path = process().input_file_path
+    
+    # Iterate over input file, calculate list of elf total calories
+    elfList = [0]
+    iterator = 0
+    with open(file_path) as inputFile:
+        for line in inputFile.readlines():
+            lineRegex = re.search("^\d+", line)
+            if lineRegex == None:
+                iterator += 1 #assuming only one new line between elves
+                elfList.append(0)
+            else:
+                elfList[iterator] = elfList[iterator] + float(lineRegex.group(0))
 
-# Print output to console
-print("Elf " + str(elfIndex+1) + " is carrying the most calories")
-        
+    # Get index of highest calorie elf
+    elfIndex = elfList.index(max(elfList))
+
+    # Print output to console
+    print("Elf " + str(elfIndex+1) + " is carrying the most calories")
+
+# Run
+if __name__ == "__main__":
+    main()
